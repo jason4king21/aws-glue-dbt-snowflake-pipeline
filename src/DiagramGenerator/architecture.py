247023152c5563ca904c0e18a127da71227b70bd
jason4king21 <jason4king21@gmail.com>
2025-05-24 13:26:41 -0500

    Diagrams

--- src/DiagramGenerator/architecture.py
+++ src/DiagramGenerator/architecture.py
@@ -1,19 +1,28 @@
 from diagrams import Diagram, Node
-from diagrams.aws.network import APIGateway
-from diagrams.aws.compute import Lambda
-from diagrams.aws.analytics import KinesisDataFirehose
+from diagrams.aws.analytics import Glue
+from diagrams.programming.language import Python
+# from diagrams.aws.compute import Lambda
+# from diagrams.aws.analytics import KinesisDataFirehose
 from diagrams.aws.storage import S3
 from diagrams.saas.analytics import Snowflake
+from diagrams.aws.security import IdentityAndAccessManagementIamAddOn
+from diagrams.aws.general import GenericDatabase
 
 
 
-with Diagram("Real-Time Streaming Pipeline", show=False, filename="../diagrams/architecture", outformat="png"):
-    api = APIGateway("API Gateway")
-    lam = Lambda("Lambda Processor")
-    firehose = KinesisDataFirehose("Firehose")
+with Diagram("AWS-glue-dbt-snowflake pipeline", show=False, filename="diagrams/architecture", outformat="png"):
+    python = Python("Extract data from an external API")
+    glue = Glue("AWS Glue")
+    iam = IdentityAndAccessManagementIamAddOn("IAM Role")
+    iam2 = IdentityAndAccessManagementIamAddOn("IAM Role")
+    raw = GenericDatabase("raw")
+    transform = GenericDatabase("transform")
+    mart = GenericDatabase("mart")
+    # lam = Lambda("Lambda Processor")
+    # firehose = KinesisDataFirehose("Firehose")
     s3 = S3("S3 Bucket")
-    s3error = S3("Error Logs")
-    snowflake = Snowflake("Snowflake")
+    # s3error = S3("Error Logs")
+    snowflake = Snowflake("GLUEDB_PRODUCTION")
 
-    api >> lam >> firehose >> s3 >> snowflake
-    lam >> s3error
+    glue >> python >> iam >> s3 >> iam2 >> snowflake >> raw >> transform >> mart
+    
